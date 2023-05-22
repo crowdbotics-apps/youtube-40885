e620711d3eb15b5d8a735e6e95d068a5a33c86eb
Crowdbotics <team@crowdbotics.com>
2023-05-22 08:07:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "youtube-40885",
+  url: "https://youtube-40885.botics.co",
+  api: "https://youtube-40885.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
